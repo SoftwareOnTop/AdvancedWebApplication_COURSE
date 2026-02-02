031a4f70621f33a29ea25af9f5b2471d969a5fdb
Aaron Mäkinen <aaron.makinen@student.lut.fi>
2026-02-02 21:04:58 +0200

    header fixed

--- week11/client/src/components/Header.tsx
+++ week11/client/src/components/Header.tsx
@@ -1,31 +1,36 @@
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import React from 'react';
+import { AppBar, Toolbar, Button, Typography, Box } from '@mui/material';
+import { Link } from 'react-router-dom';
 
-export default function ButtonAppBar() {
+const Header: React.FC = () => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
+    <AppBar position="static" sx={{ backgroundColor: '#1976d2' }}>
+      <Toolbar>
+        {}
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          MyApp
+        </Typography>
+
+        {}
+        <Box>
+          <Button 
+            color="inherit" 
+            component={Link} 
+            to="/"
           >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+            home
+          </Button>
+          <Button 
+            color="inherit" 
+            component={Link} 
+            to="/saved"
+          >
+            saved
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
-}
+};
+
+export default Header;
