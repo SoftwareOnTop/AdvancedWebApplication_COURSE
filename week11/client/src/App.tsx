6985ed71a2f3f258ed979f561a9d486b9b2b983f
Aaron Mäkinen <aaron.makinen@student.lut.fi>
2026-02-02 22:39:44 +0200

    app.tsx changed  savedpage added

--- week11/client/src/App.tsx
+++ week11/client/src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import FrontPage from './components/FrontPage';
+import SavedPage from './components/SavedPage';
+import { useJokes } from './hooks/useJokes';
+import { Container } from '@mui/material';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const { savedJokes, saveJoke, deleteJoke } = useJokes();
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <Container>
+        <Routes>
+          <Route 
+            path="/" 
+            element={<FrontPage saveJoke={saveJoke} />} 
+          />
+          <Route 
+            path="/saved" 
+            element={<SavedPage savedJokes={savedJokes} deleteJoke={deleteJoke} />} 
+          />
+        </Routes>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
